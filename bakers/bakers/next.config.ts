5b7e51e70af03aaa06290c6c2040d8b716ca8ad6
“yujin” <220yujinoh@gmail.com>
2025-11-19 00:17:28 +0900

    fix: tailwind 에러 수정

--- bakers/bakers/next.config.ts
+++ bakers/bakers/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 1. 웹팩 설정: 브라우저에서 fs 모듈을 찾을 때 무시하도록 설정
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
